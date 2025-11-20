f8f890e8f1087730141b8fb894119b9ed32286fc
Sytion06 <20060927yangste@gmail.com>
2025-11-20 00:03:39 -0500

    Removed nused imports (will add the back later)

--- src/main/java/data_access/DBTaskDataAccessObject.java
+++ src/main/java/data_access/DBTaskDataAccessObject.java
@@ -1,27 +1,9 @@
 package data_access;
 
-import com.mongodb.MongoWriteException;
-import com.mongodb.client.MongoClient;
-import com.mongodb.client.MongoClients;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoDatabase;
-import com.mongodb.client.result.UpdateResult;
-import entity.group.Group;
 import entity.task.Task;
-import entity.user.User;
-import entity.user.UserFactory;
-import use_case.change_password.ChangePasswordUserDataAccessInterface;
 import use_case.viewtasks.ViewTasksDataAccessInterface;
-import use_case.login.LoginUserDataAccessInterface;
-import use_case.logout.LogoutUserDataAccessInterface;
-import use_case.signup.SignupUserDataAccessInterface;
-import org.bson.Document;
-
 import java.util.List;
 import java.util.ArrayList;
-
-import static com.mongodb.client.model.Filters.eq;
-import static com.mongodb.client.model.Updates.set;
 
 public class DBTaskDataAccessObject implements ViewTasksDataAccessInterface {
     // Need to be implemented
