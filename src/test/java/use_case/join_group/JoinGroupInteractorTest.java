7b19e2807bf9d1ab0c2506e16bea08ee80b6c8b1
Alisa <144388364+Alisa997@users.noreply.github.com>
2025-12-01 13:13:25 -0500

    Revert "usecase for joingroup"

--- src/test/java/use_case/join_group/JoinGroupInteractorTest.java
+++ src/test/java/use_case/join_group/JoinGroupInteractorTest.java
@@ -1,91 +1,102 @@
 package use_case.join_group;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
 public class JoinGroupInteractorTest {
-    @Test
-    void successValidGroupCodeTest() {
-        String validCode = "ABCDEF";
-        JoinGroupInputData inputData = new JoinGroupInputData(validCode);
+    void successTest() {
 
-        JoinGroupUserDataAccessInterface dataAccess = new JoinGroupUserDataAccessInterface() {
+        //below is from singup input data and is completely different, be aware
+
+        /*SignupInputData inputData = new SignupInputData("Paul", "password", "password");
+        SignupUserDataAccessInterface userRepository = new InMemoryUserDataAccessObject();
+
+        // This creates a successPresenter that tests whether the test case is as we
+        // expect.
+        SignupOutputBoundary successPresenter = new SignupOutputBoundary() {
             @Override
-            public boolean groupCodeExists(String groupCode) {
-                return validCode.equals(groupCode);
+            public void prepareSuccessView(SignupOutputData user) {
+                // 2 things to check: the output data is correct, and the user has been created
+                // in the DAO.
+                assertEquals("Paul", user.getUsername());
+                assertTrue(userRepository.existsByName("Paul"));
+            }
+
+            @Override
+            public void prepareFailView(String error) {
+                fail("Use case failure is unexpected.");
+            }
+
+            @Override
+            public void switchToLoginView() {
+                // This is expected
             }
         };
 
-        JoinGroupOutputBoundary successPresenter = new JoinGroupOutputBoundary() {
-            @Override
-            public void prepareSuccessView(JoinGroupOutputData outputData) {
-                assertEquals(validCode, outputData.getGroupCode());
-            }
-
-            @Override
-            public void prepareFailView(String errorMessage) {
-                fail("Use case failure is unexpected for a valid group code.");
-            }
-        };
-
-        JoinGroupInputBoundary interactor = new JoinGroupInteractor(successPresenter, dataAccess);
+        SignupInputBoundary interactor = new SignupInteractor(userRepository, successPresenter, new UserFactory());
         interactor.execute(inputData);
     }
 
     @Test
-    void failureEmptyGroupCodeTest() {
-        JoinGroupInputData inputData = new JoinGroupInputData("");
+    void failurePasswordMismatchTest() {
+        SignupInputData inputData = new SignupInputData("Paul", "password", "wrong");
+        SignupUserDataAccessInterface userRepository = new InMemoryUserDataAccessObject();
 
-        JoinGroupUserDataAccessInterface dataAccess = new JoinGroupUserDataAccessInterface() {
+        // This creates a presenter that tests whether the test case is as we expect.
+        SignupOutputBoundary failurePresenter = new SignupOutputBoundary() {
             @Override
-            public boolean groupCodeExists(String groupCode) {
-                return false;
+            public void prepareSuccessView(SignupOutputData user) {
+                // this should never be reached since the test case should fail
+                fail("Use case success is unexpected.");
+            }
+
+            @Override
+            public void prepareFailView(String error) {
+                assertEquals("Passwords don't match.", error);
+            }
+
+            @Override
+            public void switchToLoginView() {
+                // This is expected
             }
         };
 
-        JoinGroupOutputBoundary failurePresenter = new JoinGroupOutputBoundary() {
-            @Override
-            public void prepareSuccessView(JoinGroupOutputData outputData) {
-                fail("Use case success is unexpected for an empty group code.");
-            }
-
-            @Override
-            public void prepareFailView(String errorMessage) {
-                assertEquals("Group code cannot be empty.", errorMessage);
-            }
-        };
-
-        JoinGroupInputBoundary interactor = new JoinGroupInteractor(failurePresenter, dataAccess);
+        SignupInputBoundary interactor = new SignupInteractor(userRepository, failurePresenter, new UserFactory());
         interactor.execute(inputData);
     }
 
     @Test
-    void failureInvalidGroupCodeTest() {
-        JoinGroupInputData inputData = new JoinGroupInputData("ZZZZZZ");
+    void failureUserExistsTest() {
+        SignupInputData inputData = new SignupInputData("Paul", "password", "wrong");
+        SignupUserDataAccessInterface userRepository = new InMemoryUserDataAccessObject();
 
-        JoinGroupUserDataAccessInterface dataAccess = new JoinGroupUserDataAccessInterface() {
+        // Add Paul to the repo so that when we check later they already exist
+        UserFactory factory = new UserFactory();
+        User user = factory.create("Paul", "pwd");
+        userRepository.save(user);
+
+        // This creates a presenter that tests whether the test case is as we expect.
+        SignupOutputBoundary failurePresenter = new SignupOutputBoundary() {
             @Override
-            public boolean groupCodeExists(String groupCode) {
-                return false;
+            public void prepareSuccessView(SignupOutputData user) {
+                // this should never be reached since the test case should fail
+                fail("Use case success is unexpected.");
+            }
+
+            @Override
+            public void prepareFailView(String error) {
+                assertEquals("User already exists.", error);
+            }
+
+            @Override
+            public void switchToLoginView() {
+                // This is expected
             }
         };
 
-        JoinGroupOutputBoundary failurePresenter = new JoinGroupOutputBoundary() {
-            @Override
-            public void prepareSuccessView(JoinGroupOutputData outputData) {
-                fail("Use case success is unexpected for an invalid group code.");
-            }
-
-            @Override
-            public void prepareFailView(String errorMessage) {
-                assertEquals("Invalid Group Code", errorMessage);
-            }
-        };
-
-        JoinGroupInputBoundary interactor = new JoinGroupInteractor(failurePresenter, dataAccess);
-
+        SignupInputBoundary interactor = new SignupInteractor(userRepository, failurePresenter, new UserFactory());
         interactor.execute(inputData);
+
+         */
     }
 }
